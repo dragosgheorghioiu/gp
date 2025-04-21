950f29cb3d4310467b781971d20d15a2e6dc04b7
dragosgheorghioiu <dragosgheorghioiu9@gmail.com>
2025-04-21 19:12:26 +0300

    lab3 partial solution

--- lab3/main.py
+++ lab3/main.py
@@ -1,5 +1,53 @@
 import turtle
-# Initialize turtle
+
+def apply_rules(plant):
+    axiom = plant["axiom"]
+    for _ in range(plant["iterations"]):
+        axiom = "".join([plant["rules"].get(char, char) for char in axiom])
+    return axiom
+
+def draw_l_system(t, instructions, angle, length):
+    stack = []
+    for command in instructions:
+        if command == 'F':
+            t.forward(length)
+        elif command == '+':
+            t.left(angle)
+        elif command == '-':
+            t.right(angle)
+        elif command == '[':
+            stack.append((t.position(), t.heading()))
+        elif command == ']':
+            position, heading = stack.pop()
+            t.penup()
+            t.goto(position)
+            t.setheading(heading)
+            t.pendown()
+
+length = 20
+
+plant_f = {
+    "axiom": "X",
+    "rules": {
+        "X": "F-[[X]+X]+F[+FX]-X",
+        "F": "FF"
+    },
+    "angle": 22.5,
+    "iterations": 5,
+}
+
+plant_c = {
+    "axiom": "F",
+    "rules": {
+        "F": "FF-[-F+F+F]+[+F-F-F]"
+    },
+    "angle": 22.5,
+    "iterations": 4,
+}
+
+final_string = apply_rules(plant_f)
+final_string = apply_rules(plant_c)
+
 t = turtle.Turtle()
 wn = turtle.Screen()
 t.speed(0)
@@ -8,6 +56,7 @@ t.penup()
 t.goto(0, -wn.window_height() // 2 + 20)
 t.pendown()
 
+draw_l_system(t, final_string, plant_c["angle"], length)
 
-# Wait for user to close window
 wn.mainloop()
+
